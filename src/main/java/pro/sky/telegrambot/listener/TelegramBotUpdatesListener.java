1e22d7f63a4f25a498ea37ab430a6a06c09bc0f9
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-05-22 23:58:45 +0300

    update 2

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,15 +2,11 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Chat;
-import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.service.NotificationTaskService;
@@ -18,23 +14,16 @@ import pro.sky.telegrambot.service.NotificationTaskService;
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
     private static final Pattern PATTERN = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4} \\d{2}:\\d{2})\\s(.+)");
-
-    public TelegramBotUpdatesListener(NotificationTaskService notificationTaskService) {
-        this.notificationTaskService = notificationTaskService;
-    }
-
     private final NotificationTaskService notificationTaskService;
 
     @Autowired
@@ -44,99 +33,67 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
+    public TelegramBotUpdatesListener(NotificationTaskService notificationTaskService) {
+        this.notificationTaskService = notificationTaskService;
+    }
 
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
 
-            generateWelcomeMessage(updates);
-
-            createNotificationTaskInDataBase(updates);
-
-            sendingNotification();
+            processMessage(updates);
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    public void generateWelcomeMessage(List<Update> updates) {
+    public void processMessage(List<Update> updates) {
+        List<Update> updatesForCheck = new ArrayList<>();
         updates.forEach(update -> {
             if (update.message() != null && "/start".equals(update.message().text())) {
                 String name = update.message().chat().firstName();
                 long id = update.message().chat().id();
                 SendMessage message = new SendMessage(id, "Привет, " + name + "! Отправь сообшение следующего вида " +
                         "<01.01.2022 20:00 Сделать домашнюю работу> и я обязательно напомню тебе о твоей задаче!");
-                SendResponse response = telegramBot.execute(message);
+                telegramBot.execute(message);
+            } else {
+                updatesForCheck.add(update);
             }
         });
+
+        processNotStartMessages(updatesForCheck);
     }
 
-    public boolean checkMessageToValidPattern(String text) {
-        Matcher matcher = PATTERN.matcher(text);
-        if (matcher.matches()) {
-            return true;
-        }
-        return false;
-    }
+    private void processNotStartMessages(List<Update> updates) {
+        updates.forEach(update -> {
+            Long chatId = update.message().chat().id();
+            //  Проверяем на валидность, если не валидно -- шлем сообщение
+            Matcher matcher = PATTERN.matcher(update.message().text());
+            if (!matcher.matches()) {
+                sendMessage(chatId, "Извините, но введенное вами сообщение не соответсвует шаблону!");
+                return;
+            }
+            //  Парсим, если дата меньше текущей -- шлем сообщение
+            String date = matcher.group(1);
+            String task = matcher.group(2);
+            LocalDateTime taskDt = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+            if (taskDt.isBefore(LocalDateTime.now())) {
+                sendMessage(chatId, "Извините, но введенная вами дата раньше текущей!");
+                return;
+            }
+            // Если прошли все проверки то сохраняем в БД, те вызываем createNotificationTaskEntity
+            NotificationTask notificationTask = new NotificationTask();
+            notificationTask.setIdChat(chatId);
+            notificationTask.setDate(taskDt);
+            notificationTask.setText(task);
 
-    public NotificationTask createNotificationTaskEntity(Message message) {
-        String text = message.text();
-        Matcher matcher = PATTERN.matcher(text);
-        String date = "";
-        String task = "";
-        if (matcher.matches()) {
-            date = matcher.group(1);
-            task = matcher.group(2);
-        }
-        LocalDateTime localDateTime = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-        NotificationTask notificationTask = new NotificationTask();
-        if (localDateTime.isBefore(LocalDateTime.now())) {
-            Long chatId = message.chat().id();
-            SendMessage message1 = new SendMessage(chatId, "Извините, но введенная вами дата раньше текущей!");
-            SendResponse response = telegramBot.execute(message1);
-        } else {
-            notificationTask.setDate(localDateTime);
-        }
-        notificationTask.setText(task);
-        notificationTask.setIdChat(message.chat().id());
-        return notificationTask;
-    }
-
-    public void createNotificationTaskInDataBase(List<Update> updates) {
-        List<Message> collect = updates
-                .stream()
-                .map(Update::message)
-                .filter(message -> checkMessageToValidPattern(message.text()))
-                .collect(Collectors.toList());
-        logger.info("Collect contains {}", collect.size());
-        if (collect.isEmpty()) {
-            Long chatId = updates
-                    .stream()
-                    .findFirst()
-                    .map(Update::message)
-                    .map(Message::chat)
-                    .map(Chat::id)
-                    .orElse(null);
-            SendMessage message1 = new SendMessage(chatId, "Извините, но введенное вами сообщение не соответсвует шаблону!");
-            SendResponse response = telegramBot.execute(message1);
-        }
-        collect.forEach(message -> {
-            logger.info("Send respond {}", message.text());
-            notificationTaskService.createNotificationTask(createNotificationTaskEntity(message));
+            notificationTaskService.createNotificationTask(notificationTask);
         });
     }
 
-    @Scheduled(cron = "0 * * * * *")
-    public void sendingNotification() {
-        Collection<NotificationTask> exists = notificationTaskService
-                .getCollectionNotificationTask(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
-        if (!exists.isEmpty()) {
-            exists
-                    .forEach(notificationTask -> {
-                        SendMessage message = new SendMessage(notificationTask.getIdChat(), notificationTask.getText());
-                        SendResponse response = telegramBot.execute(message);
-                    });
-        }
+    private void sendMessage(Long chatId, String msg) {
+        SendMessage tlgMsg = new SendMessage(chatId, msg);
+        telegramBot.execute(tlgMsg);
     }
 
 
